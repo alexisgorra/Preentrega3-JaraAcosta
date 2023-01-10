4ea876894b28015bc9a0ccf4504d12a52c528cb8
Alexis Jara Acosta <aleshij4@gmail.com>
2023-01-10 13:34:56 -0300

    market

--- js/main.js
+++ js/main.js
@@ -1,14 +1,66 @@
-function convertir(){
-    var valore = parseInt(document.getElementById("valor").value);
-    var resultado = 0;
-    var usd = 351;
-    var cad = 134;
-    if (document.getElementById("1").checked){
-        resultado = valore / usd;
-        alert("El Cambio de Pesos argentinos a Dolares es: $" + resultado.toFixed(3));
+const productos = [
+    {nombre: "hoodie", precio: 12000},
+    {nombre: "cargopants", precio: 7000},
+    {nombre: "remeras", precio: 5000},
+    {nombre: "zapatillas", precio: 34000},
+    {nombre: "bandana", precio: 2000},
+]
+let carrito = []
+
+let seleccion = prompt("Hola, señor Fulano, desea comprar algun producto si o no")
+
+while(seleccion !="si" && seleccion !="no"){
+    alert("por favor ingresa si o no")
+    seleccion = prompt("Hola desea comprar algo si o no")
+}
+
+if(seleccion == "si"){
+    alert("a continuacion nuestra lista de productos")
+    let todoslosProductos = productos.map((producto) => producto.nombre + " " + producto.precio + "$");
+    alert(todoslosProductos.join(" - "))
+} else if (seleccion == "no"){
+    alert("gracias por venir, hasta pronto!!")
+}
+
+    while(seleccion != "no"){
+        let producto = prompt("agrega un producto a tu carrito")
+        let precio = 0
+
+        if(producto == "hoodie" || producto == "cargopants" || producto == "remeras" || producto == "zapatillas" || producto == "bandana"){
+            switch (producto) {
+            case "hoodie":
+            precio = 12000
+            break;
+            case "cargopants":
+            precio = 7000
+            break;
+            case "remeras":
+            precio = 5000
+            break;
+            case "zapatillas":
+            precio = 34000
+            break;
+            case "bandana":
+            precio = 2000
+            break;
+            default:
+            break;
+        }
+        let unidades = parseInt(prompt("cuantas unidades quiere llevar"))
+
+        carrito.push({producto, unidades, precio})
+        console.log(carrito)
+    } else {
+        alert("no tenemos ese producto")
     }
-    if (document.getElementById("2").checked){
-        resultado = valore / cad;
-        alert("El Cambio de Pesos argentinos a Canadian Dollars es: $" + resultado.toFixed(3));
+
+    seleccion = prompt("desea seguir comprando??")
+    
+    while(seleccion == "no"){
+        alert("gracias por la compra! hasta pronto")
+        carrito.forEach((carritoFinal) =>{
+            console.log(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}, total a pagar por producto ${carritoFinal.unidades * carritoFinal.precio}`)
+        })
+        break;
     }
 }
