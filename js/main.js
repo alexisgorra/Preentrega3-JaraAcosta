d5ff55c5f7954b1a288d870b8a9b434bb1363c26
Alexis Jara Acosta <aleshij4@gmail.com>
2023-01-24 22:56:09 -0300

    Simulador de Autos

--- js/main.js
+++ js/main.js
@@ -1,66 +1,81 @@
-const productos = [
-    {nombre: "hoodie", precio: 12000},
-    {nombre: "cargopants", precio: 7000},
-    {nombre: "remeras", precio: 5000},
-    {nombre: "zapatillas", precio: 34000},
-    {nombre: "bandana", precio: 2000},
-]
-let carrito = []
-
-let seleccion = prompt("Hola, señor Fulano, desea comprar algun producto si o no")
-
-while(seleccion !="si" && seleccion !="no"){
-    alert("por favor ingresa si o no")
-    seleccion = prompt("Hola desea comprar algo si o no")
-}
-
-if(seleccion == "si"){
-    alert("a continuacion nuestra lista de productos")
-    let todoslosProductos = productos.map((producto) => producto.nombre + " " + producto.precio + "$");
-    alert(todoslosProductos.join(" - "))
-} else if (seleccion == "no"){
-    alert("gracias por venir, hasta pronto!!")
-}
-
-    while(seleccion != "no"){
-        let producto = prompt("agrega un producto a tu carrito")
-        let precio = 0
-
-        if(producto == "hoodie" || producto == "cargopants" || producto == "remeras" || producto == "zapatillas" || producto == "bandana"){
-            switch (producto) {
-            case "hoodie":
-            precio = 12000
-            break;
-            case "cargopants":
-            precio = 7000
-            break;
-            case "remeras":
-            precio = 5000
-            break;
-            case "zapatillas":
-            precio = 34000
-            break;
-            case "bandana":
-            precio = 2000
-            break;
-            default:
-            break;
-        }
-        let unidades = parseInt(prompt("cuantas unidades quiere llevar"))
-
-        carrito.push({producto, unidades, precio})
-        console.log(carrito)
-    } else {
-        alert("no tenemos ese producto")
+let cars = [
+    { model: "Honda Civic", year: 2020, price: 25000 },
+    { model: "Toyota Camry", year: 2019, price: 30000 },
+    { model: "Ford Mustang", year: 2018, price: 35000 },
+    { model: "BMW 03 E36M3 lightweight", year: 1980, price: 100000 },
+    { model: "Ford Escort Cosworth", year: 1960, price: 90000 },
+    { model: "BMW Touring '74", year: 2002, price: 70000 },
+  ];
+  //Funciones
+  function viewCars() {
+    console.log("Lista de automóviles:");
+    for (let i = 0; i < cars.length; i++) {
+      console.log(`${i + 1}. ${cars[i].model} (${cars[i].year}) - $ ${cars[i].price}`);
     }
-
-    seleccion = prompt("desea seguir comprando??")
-    
-    while(seleccion == "no"){
-        alert("gracias por la compra! hasta pronto")
-        carrito.forEach((carritoFinal) =>{
-            console.log(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}, total a pagar por producto ${carritoFinal.unidades * carritoFinal.precio}`)
-        })
+  }
+  
+  function addCar() {
+    let newModel = prompt("Ingrese el modelo del automóvil:");
+    let newYear = prompt("Ingrese el año del automóvil:");
+    let newPrice = prompt("Ingrese el precio del automóvil:");
+    cars.push({ model: newModel, year: newYear, price: newPrice });
+    console.log(`Automóvil ${newModel} agregado exitosamente.`);
+  }
+  
+  // Función para modificar un auto existente y eliminar un auto del arreglo
+  function updateCar() {
+    let carIndex = prompt("Ingrese el número del automóvil que desea modificar:");
+    let updatedModel = prompt("Ingrese el nuevo modelo del automóvil:");
+    let updatedYear = prompt("Ingrese el nuevo año del automóvil:");
+    let updatedPrice = prompt("Ingrese el nuevo precio del automóvil:");
+    cars[carIndex - 1] = { model: updatedModel, year: updatedYear, price: updatedPrice };
+    console.log(`Automóvil ${updatedModel} modificado exitosamente.`);
+  }
+  
+  function deleteCar() {
+    let carIndex = prompt("Ingrese el número del automóvil que desea eliminar:");
+    let deletedCar = cars.splice(carIndex - 1, 1);
+    console.log(`Automóvil ${deletedCar[0].model} eliminado exitosamente.`);
+  }
+  
+  // Bucle para mostrar el menú y manejar las acciones seleccionadas
+  while (true) {
+    let menuOption = prompt("Seleccione una opción:\n1. Ver automóviles\n2. Agregar automóvil\n3. Modificar automóvil\n4. Eliminar automóvil\n5. Salir");
+  
+    if (menuOption == 1) {
+        viewCars();
+    } else if (menuOption == 2) {
+        addCar();
+    } else if (menuOption == 3) {
+        updateCar();
+    } else if (menuOption == 4) {
+        deleteCar();
+    } else if (menuOption == 5) {
         break;
+    } else {
+        console.log("Opción inválida, seleccione una opción válida.");
     }
-}
+  }
+  // Objetos
+  class Vehiculos {
+    constructor(modelo, marca, color) {
+      this.modelo = modelo;
+      this.marca = marca;
+      this.color = color;
+      this.confort = function () {
+        console.log(
+          "Todos los autos estan tuneados."
+        );
+      };
+    }
+  }
+  const vehiculo1 = new Vehiculos("T66", "Toyota Supra", "Naranja");
+  const vehiculo2 = new Vehiculos("R34", "Nissan Skyline GT-R", "Gris");
+  const vehiculo3 = new Vehiculos("RS", "Mitsubishi Eclipse", "Verde");
+  const vehiculo4 = new Vehiculos("370Z", "El Nissan", "Gris");
+  
+  console.log(vehiculo1, vehiculo2, vehiculo3);
+  vehiculo1.confort();
+  vehiculo2.confort();
+  vehiculo3.confort();
+  vehiculo4.confort();
